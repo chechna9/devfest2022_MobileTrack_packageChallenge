394b3883af9f02c4b00a872fccdf7711cd74a1c9
chechna9 <ja_bouchouareb@esi.dz>
2022-11-30 11:21:49 +0100

    delete the test package

--- flutter_pkg/lib/flutter_pkg.dart
+++
@@ -1,3 +0,0 @@
-export 'package:flutter_pkg/group.dart';
-export 'package:flutter_pkg/individual_modal.dart';
-export 'package:flutter_pkg/individuel_card.dart';
--- flutter_pkg/lib/individual_modal.dart
+++
@@ -1,5 +0,0 @@
-class Individualmodel {
-  final String imageUrl;
-  final bool isNet;
-  Individualmodel({required this.imageUrl, required this.isNet});
-}
